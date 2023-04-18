ae10b7d77c74c37c4a845ff5ea63408a59794071
AMugari <amugari@wcyber.net>
2023-04-18 19:53:29 +0200

    Abe Commit

---
+++ Abe Data Cleaning and Description.R
@@ -0,0 +1,119 @@
+library(readxl)
+
+# Install pacman ("package manager") if needed
+if (!require("pacman")) install.packages("pacman")
+
+#Loading the Data from the excel sheet
+pacman::p_load(pacman, party, psych, rio, tidyverse)
+tax_data <- read_excel("Taxpayer Data Consolidated.xlsx")
+
+head(tax_data)
+#DATA CLEANING FOR THE DATE
+
+# Convert "Return Date" column to Date format
+tax_data$`Return. Date` <- as.Date(tax_data$`Return. Date`, format = "%d.%m.%Y")
+
+# Convert "Due Date" column to Date format
+tax_data$`Due Date`<- as.Date(tax_data$`Due Date`, format = "%d.%m.%Y")
+
+tax_data$`Date Paid`<- as.Date(tax_data$`Date Paid`, format = "%d.%m.%Y")
+
+head(tax_data)
+
+
+
+
+
+
+
+hist(tax_data$`Amount Due`, 
+     main = "Distribution of Amount Due", 
+     xlab = "Amount Due", 
+     ylab = "Frequency",
+     breaks = 60)
+
+# Create a subset of the data for 5 different taxpayers
+plot_data <- subset(tax_data, tax_data$`ID#` %in% c("BP278"))
+
+head(plot_data)
+# Create a scatterplot
+plot(plot_data$`Amount Due`, plot_data$`Amount Paid`, 
+     xlab = "Amount Due", ylab = "Amount Paid", 
+     main = "Scatterplot of Amount Due vs. Amount Paid for 5 taxpayers")
+
+
+
+library(ggplot2)
+
+# Create a new column "Compliance" that indicates whether the taxpayer is compliant or not
+tax_data$Compliance <- ifelse(tax_data$`Date Paid` <= tax_data$`Due Date`, "Compliant", "Non-Compliant")
+
+# Create a pie chart of taxpayer compliance
+ggplot(data = tax_data, aes(x = "", fill = Compliance)) +
+  geom_bar(width = 1) +
+  coord_polar("y", start=0) +
+  theme_void() +
+  labs(title = "Taxpayer Compliance")
+
+tax_data$Amount_Paid <- tax_data$`Amount Due`- tax_data$`Amount Paid`
+
+summary(tax_data$Taxhead)
+# Use the table function to get a frequency table of the Taxhead column
+table(tax_data$Taxhead)
+
+# Create a bar graph of the Taxhead column
+ggplot(tax_data, aes(x = Taxhead)) +
+  geom_bar(fill = "lightblue") +
+  labs(title = "Distribution of Tax Heads", x = "Tax Head", y = "Count")
+
+# Create a distribution plot of amount paid
+ggplot(data = tax_data, aes(x = Amount_Paid)) +
+  geom_density(fill = "lightblue", alpha = 0.7) +
+  geom_vline(aes(xintercept = mean(Amount_Paid)), color = "red", linetype = "dashed", size = 1) +
+  labs(title = "Distribution of Amount Paid", x = "Amount Paid", y = "Density")
+
+
+# Create a histogram of "Amount Paid"
+hist(tax_data$`Amount Paid`, breaks = 10, xlab = "Amount Paid", ylab = "Frequency")
+
+#Data Cleaning 
+#Removing Null values
+sum(is.na(tax_data))
+tax_data <- na.omit(tax_data)
+
+# Remove duplicate rows from the data frame
+tax_data <- distinct(tax_data)
+
+# Check the data types of each column
+str(tax_data)
+
+#  box plot of the Amount Paid column to check for outliers
+ggplot(tax_data, aes(y = `Amount Paid`)) +
+  geom_boxplot()
+head(tax_data)
+
+# Create a new column "Compliance" that indicates whether the taxpayer is compliant or not
+tax_data$Compliance <- ifelse(tax_data$`Date Paid` <= tax_data$`Due Date`, "Compliant", "Non-Compliant")
+
+# Create a pie chart of taxpayer compliance
+ggplot(data = tax_data, aes(x = "", fill = Compliance)) +
+  geom_bar(width = 1) +
+  coord_polar("y", start=0) +
+  theme_void() +
+  labs(title = "Taxpayer Compliance")
+
+tax_data$Amount_Paid <- tax_data$`Amount Due`- tax_data$`Amount Paid`
+
+summary(tax_data$Taxhead)
+# Use the table function to get a frequency table of the Taxhead column
+table(tax_data$Taxhead)
+
+# Create a bar graph of the Taxhead column
+ggplot(tax_data, aes(x = Taxhead)) +
+  geom_bar(fill = "lightblue") +
+  labs(title = "Distribution of Tax Heads", x = "Tax Head", y = "Count")
+
+
+ggplot(tax_data, aes(x = tax_data$`Amount Due`, y = tax_data$`Amount Paid`)) +
+  geom_point() +
+  geom_smooth(method = "lm")
